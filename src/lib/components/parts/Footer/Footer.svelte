08d50b960ee0c7d5df3b46caead76d57dcfa0e27
もいずる <momizibe@outlook.jp>
2025-11-14 01:56:45 +0900

    デザインの調整

--- src/lib/components/parts/Footer/Footer.svelte
+++ src/lib/components/parts/Footer/Footer.svelte
@@ -1,4 +1,11 @@
-<div class="fixed flex-col-center bottom-0 left-0 m-5 text-lg z-100001 bg-primary/3 backdrop-blur-sm drop-shadow-sm drop-shadow-primary">
-    <p class="pointer-events-none">作者:玉木</p>
-    <a href="https://github.com/moizlu/image-editor-web" target="_blank">ソースコード</a>
+<script lang="ts">
+  import Icon from "$lib/components/ui/Icon/Icon.svelte";
+</script>
+
+<div class="fixed flex-col-center bottom-0 left-0 m-5 text-lg z-100001 bg-primary/3 backdrop-blur-sm drop-shadow-sm drop-shadow-primary pointer-events-none">
+    <p class="m-1">作者:玉木</p>
+    <a class="flex-center pointer-events-auto" href="https://github.com/moizlu/image-editor-web" target="_blank">
+        <p class="m-1">ソースコード</p>
+        <Icon lightSrc="/images/light/ex_link.svg" darkSrc="/images/dark/ex_link.svg" alt="link" width={20} class="m-2 pointer-events-none drop-shadow-sm drop-shadow-accent" />
+    </a>
 </div>
